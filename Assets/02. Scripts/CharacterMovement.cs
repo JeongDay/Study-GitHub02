9e2dffbd5314fdab0f4b51ca6387f44806826997
JeongDay <cidd1332@gmail.com>
2025-05-19 23:26:41 +0900

    Init
    Init Settings

---
+++ Assets/02. Scripts/CharacterMovement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public SoundManager soundManager;
+    
+    public SpriteRenderer[] renderers;
+    private Rigidbody2D characterRb;
+
+    public float moveSpeed = 5f;
+    public float jumpPower = 5f;
+    private float h;
+
+    public bool isGround;
+
+    void Start()
+    {
+        renderers = this.GetComponentsInChildren<SpriteRenderer>(true);
+        characterRb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGround) // isGround == true
+            {
+                soundManager.OnJumpSound();
+                characterRb.AddForceY(jumpPower, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (h != 0)
+        {
+            renderers[0].gameObject.SetActive(false);
+            renderers[1].gameObject.SetActive(true);
+            characterRb.linearVelocityX = h * moveSpeed;
+
+            if (h > 0)
+            {
+                renderers[0].flipX = false;
+                renderers[1].flipX = false;
+            }
+            else if (h < 0)
+            {
+                renderers[0].flipX = true;
+                renderers[1].flipX = true;
+            }
+        }
+        else
+        {
+            renderers[0].gameObject.SetActive(true);
+            renderers[1].gameObject.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGround = true;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGround = false;
+    }
+}
---
+++ Assets/02. Scripts/PinballController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PinballController : MonoBehaviour
+{
+    public Rigidbody2D leftStickRb;
+    public Rigidbody2D rightStickRb;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            leftStickRb.AddTorque(30f);
+        else
+            leftStickRb.AddTorque(-25f);
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+            rightStickRb.AddTorque(-30f);
+        else
+            rightStickRb.AddTorque(25f);
+    }
+}
